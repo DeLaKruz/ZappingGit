03edcb336b6110f9a0494be02614f076aeab804b
DeLaKruz <yerayg466@gmail.com>
2023-04-27 18:10:01 +0200

    Test canal origen: 5 canal destino 63: Salida: 41 clics perdon, se me olvido meter los cambios del main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,10 @@ package org.example;
 
 public class Main {
         public static int cambioDeCanalOptimo(int canaldeorigen, int canaldedestino) {
-            int difCanales = Math.abs(canaldedestino - canaldeorigen);
-            int clicsDirectos = Math.min(difCanales, 99 - difCanales);
-            int clicsIndirectos = 1 + Math.min(canaldeorigen, canaldedestino) + (99 - Math.max(canaldeorigen, canaldedestino));
-            return Math.min(clicsDirectos, clicsIndirectos);
+            int distancia = Math.abs(canaldedestino - canaldeorigen);
+            if (distancia > 49) {
+                distancia = 99 - distancia;
+            }
+            return distancia;
         }
     }
